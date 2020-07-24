7163976c98dbdaafadd149dab1526a31dc66c706
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2020-07-24 23:11:35 +0530

    leetcode - addBinary

--- cp.cpp
+++ cp.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1170/
+// https://leetcode.com/explore/learn/card/array-and-string/203/introduction-to-string/1160/
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
@@ -10,40 +10,77 @@ using namespace std;
 class Solution
 {
 public:
-    vector<vector<int>> generate(int numRows)
+    string addBinary(string a, string b)
     {
         SPEED;
-        vector<vector<int>> pascals_triangle;
-        if (numRows)
+        if (a == "0")
+            return b;
+        else if (b == "0")
+            return a;
+        string sum = "";
+        pair<string, string> small_large = a.size() > b.size() ? pair<string, string>(b, a) : pair<string, string>(a, b);
+        bool carry = false;
+        int i, j;
+        int small_size = small_large.first.size();
+        int large_size = small_large.second.size();
+        for (i = small_size - 1, j = large_size - 1; i >= 0 && j >= 0; --i, --j)
         {
-            for (int row_size = 1; row_size <= numRows; ++row_size)
+            if (small_large.first[i] == '1' && small_large.second[j] == '1')
             {
-                pascals_triangle.push_back(vector<int>(row_size, 1));
-                int row_index = row_size - 1;
-
-                for (int i = 1; i < row_size - 1; ++i)
-                {
-                    pascals_triangle[row_index][i] = pascals_triangle[row_index - 1][i - 1] + pascals_triangle[row_index - 1][i];
-                }
+                if (carry)
+                    sum = '1' + sum;
+                else
+                    sum = '0' + sum;
+                carry = true;
+            }
+            else if (small_large.first[i] == '1' || small_large.second[j] == '1')
+            {
+                if (carry)
+                    sum = '0' + sum;
+                else
+                    sum = '1' + sum, carry = false;
+            }
+            else
+            {
+                if (carry)
+                    sum = '1' + sum;
+                else
+                    sum = '0' + sum;
+                carry = false;
             }
         }
-        return pascals_triangle;
+        int remain = i >= 0 ? i : j;
+        if (remain >= 0)
+            for (; remain >= 0; --remain)
+            {
+                if (small_large.second[remain] == '1')
+                {
+                    if (carry)
+                        sum = '0' + sum;
+                    else
+                        sum = '1' + sum;
+                }
+                else
+                {
+                    if (carry)
+                        sum = '1' + sum, carry = false;
+                    else
+                        sum = '0' + sum;
+                }
+            }
+
+        if (carry)
+            sum = '1' + sum;
+        return sum;
     }
 };
 int main()
 {
-    int m;
-    cin >> m;
+    string a, b;
+    cin >> a >> b;
+    string sum = Solution().addBinary(a, b);
 
-    vector<vector<int>>
-        pascals_triangle = Solution().generate(m);
-    for (auto row : pascals_triangle)
-    {
-        for (int e : row)
-            cout << e << " ";
-        cout << "\n";
-    }
-    cout << "\n";
+    cout << sum << "\n";
 
     return 0;
 }
